04d108d873fa446ede304f89f6262fe667de004e
gdukes707 <Gdukes707@gmail.com>
2020-06-21 08:33:07 -0700

    first three pages complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,111 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 const Projects = () => {
   return (
     <div className="homePage">
-      <section className="homeSection1">This is section one</section>
-      <section className="homeSection2">This is section two</section>
-      <section className="homeSection3">This is section three</section>
+      <section className="homeSection1">
+        <div className="e-comm">
+          <h3>E-comm</h3>
+          <ReactPlayer url="https://youtu.be/n_6DL060IlA" />
+          <span>
+            <bold>Technologies:</bold>
+            <br></br>
+            React, Node.js, MongoDb, Mongoose, Bcrypt, Cloudinary, Stripe,
+            Electron, Bootstrap
+          </span>
+          <br></br>
+          <span>
+            - Solo final project where I created a mock small business store
+            front that utilized a react(hooks), stripe and bootstrap to create
+            an end client focused site with a live rendering and persisting cart
+            to increase satisfaction in online shopping and integrated Stripe
+            payments.
+          </span>
+          <br></br>
+          <span>
+            - Built an Electron App that is utilized for store administration,
+            product creation and Admin profile creation. This app also utilized
+            a React front end and integrated the cloudinary-react api.
+          </span>
+          <br />
+          <span> - Made a MEN stack api to deliver store data in JSON.</span>
+          <br></br>
+          <span>
+            See the Repo <br></br>
+            <a href="https://git.generalassemb.ly/users/gdukes707/projects/2">
+              https://git.generalassemb.ly/users/gdukes707/projects/2
+            </a>
+            , <br></br> Live version is currently not operating
+          </span>
+        </div>
+      </section>
+      <section className="homeSection2">
+        <div className="movieBook">
+          <h3>Movie Book</h3>
+          <ReactPlayer url="https://youtu.be/hi6ze0kFqaE" />
+          <span>
+            <bold>Technologies:</bold>
+            <br></br>
+            Django, Bootstrap, AWS S3, & TMDb api
+          </span>
+          <br></br>
+          <span>
+            - Teamed with 2 other engineers to create a movie suggestion and
+            review site.
+          </span>
+          <br></br>
+          <span>
+            - Was in charge of creating a 4 full crud model database with
+            authorization and integration of that and the tmdb api into
+            templates
+          </span>
+          <br />
+          <span> - Built in under a week.</span>
+          <br></br>
+          <span>
+            See the Live version here <br></br>
+            <a href="https://movie-book-one.herokuapp.com/">
+              https://movie-book-one.herokuapp.com/
+            </a>
+          </span>
+        </div>
+      </section>
+      <section className="homeSection4">
+        <div className="pokeTracker">
+          <h3>PokeTracker</h3>
+          <ReactPlayer url="https://youtu.be/jIQdY1wiXQQ" />
+          <span>
+            <bold>Technologies:</bold>
+            <br></br>
+            Node.js EJS Mongoose MongoDB, Bcrypt & Pokemon api
+          </span>
+          <br></br>
+          <span>
+            - Partnered with another engineer to create a Pokedex clone that
+            utilized authorization to create users that could store Pokemon they
+            catch in any of the first 6 games.
+          </span>
+          <br></br>
+          <span>
+            - Was in charge of creating a 3 full crud model backend with api
+            integrating and some templating via ejs
+          </span>
+          <br />
+          <span>
+            {" "}
+            - Was first experience with backend work and working with
+            authorization.
+          </span>
+          <br></br>
+          <span>
+            See the Live version here <br></br>
+            <a href="https://poke-tracker-pokedex.herokuapp.com/">
+              https://poke-tracker-pokedex.herokuapp.com/
+            </a>
+          </span>
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,10 +2,87 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div className="homePage">
-      <section className="homeSection1">This is section one</section>
-      <section className="homeSection2">This is section two</section>
-      <section className="homeSection3">This is section three</section>
+    <div className="resumePage">
+      <section className="homeSection1">
+        <div className="devSkills">
+          <h3>Developer Skills</h3>
+          <span>
+            <bold>Languages & Frameworks: </bold>
+            HTML, CSS, Javascript, Express, jQuery, Django, React, Node
+          </span>
+          <br></br>
+          <span>
+            <bold>Management & Deployment: </bold>
+            Github, Git, Command Line, Heroku
+          </span>
+          <br></br>
+          <span>
+            <bold>Databases: </bold>
+            PSQL, MongoDB
+          </span>
+          <br></br>
+          <span>
+            <bold>Methodologies: </bold>
+            Object Oriented Programming, MVC Pattern, User Stories, Functional
+            Programming
+          </span>
+          <br></br>
+        </div>
+      </section>
+      <section className="homeSection2">
+        {" "}
+        <div className="proExperience">
+          <h3>Professional Experience</h3>
+          <span>
+            <bold>Private Client Banker, JPMorgan Chase & Co; 8/19-3/20 </bold>
+            <br></br>
+            Analyzed complex financial information and distilling it into easy
+            to understand recommendations and educating them on sound financial
+            policy.
+          </span>
+          <br></br>
+          <span>
+            <bold>Financial Representative, Northwestern Mutual; </bold>
+            <br></br>
+            Analyzed complex information and distilling it into easy to
+            understand recommendations and educating them on sound financial
+            policy with a focus towards risk mitigation
+          </span>
+          <br></br>
+          <span>
+            <bold>Consultant, Dorjil company;</bold>
+            <br></br>
+            Performed & Analyzed market research & developed a marketing plan
+            and Delivered these insights to the key stakeholders.
+          </span>
+          <br></br>
+          <span>
+            <bold>Consultant, Altered Art Studios; </bold>
+            <br></br>
+            Performed Market research and Helped with graphic design/ logo
+            design.
+          </span>
+          <br></br>
+        </div>
+      </section>
+      <section className="homeSection4">
+        <div className="education">
+          <h3>Education</h3>
+          <span>
+            <bold>General Assembly Software Engineering Fellow:</bold>
+            <br />A 550 hour fellowship where we study all aspects of full stack
+            development. Here I built full stack web applications in 2 languages
+            and 3 frameworks. The most important skill they helped hone was the
+            ability to learn but also to teach yourself.
+          </span>
+          <br></br>
+          <span>
+            <bold>Santa Rosa Junior College:</bold>
+            <br /> Completed coursework in marketing, sales, advirtisement,
+            entrpeneurship and religious studies.
+          </span>
+        </div>
+      </section>
     </div>
   );
 };
